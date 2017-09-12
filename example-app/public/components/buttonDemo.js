9fe1a90b769bda20561f79bb5c8e22723896d246
Chip <chip4@users.noreply.github.com>
2017-09-12 11:34:05 -0700

    use buttonFactory to create different button types

--- example-app/public/components/buttonDemo.js
+++ example-app/public/components/buttonDemo.js
@@ -1,8 +1,13 @@
 import yo from '../externals/yo-yo.js';
-import button from '../../../button/index.js';
+import button, { raisedButton, buttonFactory } from '../../../material-components/button/index.js';
 
 export default () => {
   return yo`
-    ${button()}
+    <div>
+      ${button(yo`<i>Flat Button</i>`)}
+      ${raisedButton('Raised Button')}
+      ${button({ onclick: (event) => console.log(event) },'On Click')}
+      ${buttonFactory(['compact', 'dense'])('Compact and Dense')}
+    </div>
   `;
 };
--- material-components/button/index.js
+++ material-components/button/index.js
@@ -1,14 +1,28 @@
-import 'https://wzrd.in/standalone/bel@latest';
-import { ripple } from '../vendor/material-components-web.js';
+import '../vendor/bel@5.1.1.js';
+import { ripple } from '../vendor/material-components-web@0.20.0.js';
 
-console.log("ripple",ripple);
-
-export default () => {
-  const el = bel`
-    <button class="mdc-button mdc-button--raised">
-      Raised button
-    </button>
-  `;
-  ripple.MDCRipple.attachTo(el);
-  return el;
-};
+/**
+ * Button Factory
+ *
+ * @export buttonFactory
+ * @param {Array} modifiers array of material-components-web button modifiers
+ * @returns button creation function that creates buttons with given modifiers
+ */
+export function buttonFactory(modifiers=[]) {
+  return (props, ...children) => {
+    console.log('props',typeof props, props instanceof HTMLElement);
+    if(typeof props !== 'object' || props instanceof HTMLElement) {
+      children.push(props);
+      props = undefined;
+    }
+    const el = bel`
+      <button class="mdc-button ${modifiers.map((mod) => `mdc-button--${mod}`).join(' ')}" ${props}>
+        ${children}
+      </button>
+    `;
+    ripple.MDCRipple.attachTo(el);
+    return el;
+  };
+}
+export const raisedButton = buttonFactory(['raised']);
+export default buttonFactory();
