6bd5a2048b6b9df74c1f2ebd4107cacfceec6f2e
Chip <chip4@users.noreply.github.com>
2017-09-16 15:58:10 -0700

    added elevation mixins for use with styled-elements

--- example-app/public/index.js
+++ example-app/public/index.js
@@ -3,17 +3,27 @@ import styled from './externals/styled-elements.js';
 import buttonDemo from './components/buttonDemo.js';
 import textFieldDemo from './components/textFieldDemo.js';
 import elevationDemo from './components/elevationDemo.js';
-import withElevation from '../../material-components/elevation.js';
+import withElevation, { elevationMixin, elevationTransitionMixin } from '../../material-components/elevation.js';
 
 const paper = (...children) => styled(withElevation(2)(yo`<div>${children}</div>`))`
   padding: 1em;
   margin: 1em;
 `;
 
+const newPaper = styled.div`
+  padding: 1em;
+  margin: 1em;
+  ${elevationMixin(4)}
+  ${elevationTransitionMixin()}
+  &:active {
+    ${elevationMixin(8)}
+  }
+`;
+
 const root = (state = {} , dispatch) => {
   return yo`
     <div>
-      ${paper(buttonDemo())}
+      ${newPaper(buttonDemo())}
       ${paper(textFieldDemo())}
       ${paper(elevationDemo())}
     </div>
